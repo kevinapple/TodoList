4e42abf3ad766d535556c6dce81de22b819b2cf2
kevinwit <kevin.zou@honeywell.com>
2019-08-05 18:18:21 +0800

    Updates

---
+++ precache-manifest.4534109a312671f57cc196e9af1bf2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e8c95d1d816cc1cb027",
+    "url": "/todoList/static/js/runtime~main.7e8c95d1.js"
+  },
+  {
+    "revision": "bcf520282966f0462077",
+    "url": "/todoList/static/js/main.bcf52028.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/todoList/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "bcf520282966f0462077",
+    "url": "/todoList/static/css/main.901da0b9.chunk.css"
+  },
+  {
+    "revision": "40863b095e0e6d958738940e7d1757d9",
+    "url": "/todoList/index.html"
+  }
+];
